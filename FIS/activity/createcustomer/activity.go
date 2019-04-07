0d2d5fc5ddb7285bd58902a89c89ea31df1ba5a0
Santosh <bhutkar.santosh@gmail.com>
2019-04-07 22:57:52 +0530

    h

--- FIS/activity/createcustomer/activity.go
+++ FIS/activity/createcustomer/activity.go
@@ -3,7 +3,7 @@
  * This file is subject to the license terms contained
  * in the license file that is distributed with this file.
  */
-package cratecustomer
+package createcustomer
 
 import (
 	"encoding/base64"
